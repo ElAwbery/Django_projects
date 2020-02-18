a06d9de40f6fea0b693e58347f8b3c5738a01e1c
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2020-02-18 13:58:24 -0800

    added commentary about 'application' callable

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,9 @@
 """
 WSGI config for charlieawbery (config module) project.
+WSGI works by defining a callable object (a function as variable) called 
+'application' inside this WSGI file. This callable expects a request object, 
+which the WSGI server provides; and returns a response object, which the 
+WSGI server serializes and sends to the client.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
