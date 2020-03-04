05df3d95e453004ea17a2ed4537984fe2bf5952a
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2020-03-03 18:33:24 -0800

    Delete views.py

--- charlieawbery_root/views.py
+++
@@ -1,6 +0,0 @@
-# Django views source data from databases and deliver it to templates
-# Each view performs a specific function and has an associated template
-# A view can be represented by either a function or a class method
-from django.shortcuts import render
-
-# Create your views here.
